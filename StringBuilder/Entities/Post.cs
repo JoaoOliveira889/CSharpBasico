e7a6c2a031813f43aaa24fc758ddb4b87eccd6dc
João Paulo de Oliveira <jpoliveira8809@outlook.com>
2022-01-25 10:22:55 -0300

    Herança e StringBuilder

---
+++ StringBuilder/Entities/Post.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace UseStringBuilder.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();           
+            sb.AppendLine(Title);//Adiciona texto no fim do StringBuilder e adiciona uma quebra de linha
+            sb.Append(Likes);//Adiciona texto no fim do StringBuilder
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ StringBuilder/Program.cs
@@ -0,0 +1,28 @@
+﻿using UseStringBuilder.Entities;
+
+Comment c1 = new("Have a nice trip!");
+Comment c2 = new("Wow that's awesome!");
+
+Post p1 = new(
+    DateTime.Parse("21/06/2018 13:05:44"),
+    "Traveling to New Zealand",
+    "I'm going to visit this wonderful country!",
+    12);
+
+p1.AddComment(c1);
+p1.AddComment(c2);
+
+Comment c3 = new("Good night");
+Comment c4 = new("May the Force be with you");
+
+Post p2 = new(
+    DateTime.Parse("24/08/2020 11:06:42"),
+    "Good night guys",
+    "See you tomorrow",
+    5);
+
+p2.AddComment(c3);
+p2.AddComment(c4);
+
+Console.WriteLine(p1);
+Console.WriteLine(p2);
