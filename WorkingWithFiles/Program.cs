3f9efcb4d082e0bf5cfd785de90713279ef6175e
João Paulo Oliveira <joao8809@icloud.com>
2022-02-04 16:33:20 -0300

    Trabalhando com arquivos

---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,101 @@
+﻿string sourcePath = @"C:\Users\jpoli\source\repos\CSharpBasico\WorkingWithFiles\file1.txt";
+string targetPath = @"C:\Users\jpoli\source\repos\CSharpBasico\WorkingWithFiles\file2.txt";
+
+#region FileInfo
+//try
+//{
+//    FileInfo fileInfo = new(sourcePath);
+//    fileInfo.CopyTo(targetPath);
+
+//    string[] lines = File.ReadAllLines(sourcePath);
+
+//    foreach (string line in lines)
+//    {
+//        Console.WriteLine(line);
+//    }
+
+//}catch (IOException ex)
+//{
+//    Console.WriteLine("An error occurred");
+//    Console.WriteLine(ex.Message);
+//}
+#endregion
+#region FileStream e StreamReader
+//try
+//{
+//    using (StreamReader sr = File.OpenText(sourcePath))
+//    {
+//        while (!sr.EndOfStream)
+//        {
+//            string line = sr.ReadLine();
+//            Console.WriteLine(line);
+//        }
+//    }
+//}
+//catch (IOException ex)
+//{
+//    Console.WriteLine("An error accurred");
+//    Console.WriteLine(ex.Message);
+//}
+#endregion
+#region StreamWriter
+//try
+//{
+//    string[] lines = File.ReadAllLines(sourcePath);
+//    using StreamWriter sw = File.AppendText(targetPath);
+//    foreach (string line in lines)
+//    {
+//        sw.WriteLine(line.ToUpper());
+//    }
+//}
+//catch (IOException ex)
+//{
+
+//    Console.WriteLine("An error occurred");
+//    Console.WriteLine(ex.Message);
+//}
+#endregion
+#region Directory e DirectoryInfo
+//string path = @"C:\Users\jpoli\source\repos\CSharpBasico\WorkingWithFiles\myfolder";
+
+//try
+//{
+//    //Lista Pastas
+//    var folder = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+
+//    Console.WriteLine("Folders:");
+//    foreach (string s in folder)
+//    {
+//        Console.WriteLine(s);
+//    }
+
+//    //Lista arquivos
+//    var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+
+//    Console.WriteLine("Files:");
+//    foreach (string s in files)
+//    {
+//        Console.WriteLine(s);
+//    }
+
+//    //Cria uma pasta
+//    Directory.CreateDirectory(path + @"\newfolder");
+//}
+//catch (IOException ex)
+//{
+
+//    Console.WriteLine("An error occurred");
+//    Console.WriteLine(ex.Message);
+//}
+#endregion
+#region Path
+string path = @"C:\Users\jpoli\source\repos\CSharpBasico\WorkingWithFiles\file1.txt";
+Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+#endregion
