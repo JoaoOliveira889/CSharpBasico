f719671d622984ae667b95c1887088d533f52bad
João Paulo Oliveira <joao8809@icloud.com>
2022-02-21 16:41:24 -0300

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿Dictionary<string, string> cookies = new();
+cookies["user"] = "joão";
+cookies["email"] = "joao@email.com";
+cookies["phone"] = "9999999";
+cookies["phone"] = "888888";
+
+
+Console.WriteLine(cookies["phone"]);
+Console.WriteLine(cookies["email"]);
+
+cookies.Remove("email");
+
+if (cookies.ContainsKey("email"))
+{
+    Console.WriteLine(cookies["email"]);
+}
+else
+{
+    Console.WriteLine("There is no 'email' key");
+}
+
+Console.WriteLine("size " + cookies.Count);
+
+Console.WriteLine("All cookies");
+foreach (var item in cookies)
+{
+    Console.WriteLine(item.Key + ": " + item.Value);
+}
